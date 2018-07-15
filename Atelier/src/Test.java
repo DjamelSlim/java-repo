4545bde4599b74ebcc3baea02b49c2ea8d46d959
DjamelSlim <slimane.zeggari@gmail.com>
2018-07-15 19:47:43 +0200

    commit sur develop

--- Atelier/src/Test.java
+++ Atelier/src/Test.java
@@ -2,7 +2,17 @@
 public class Test {
 	
 	private String tata;
-	
+
 	private String toto;
+
+	private String methode(){
+
+		return "correction";
+	}
+
+	public  String methodInLoacal(){
+
+	    return "local";
+    }
 
 }
