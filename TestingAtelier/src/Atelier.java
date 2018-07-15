780f485f2915f751344cf264a69824e84ee74f74
DjamelSlim <slimane.zeggari@gmail.com>
2018-07-15 14:00:48 +0200

    modification du nom de la méthode

--- TestingAtelier/src/Atelier.java
+++ TestingAtelier/src/Atelier.java
@@ -8,14 +8,14 @@ public class Atelier {
 	
 
 	
-	public void countPartipant(){
+	public void countParPartipant(){
 		System.out.println("modification dans la branche distante");
 		System.out.println();
 		System.out.println("modification en local");
 		
 	}
 	//commentaire
-	public void reset(){
+	public void reset1(){
 		
 	}
 
