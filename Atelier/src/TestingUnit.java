b2578db62aa6d15f426b17a6d99cda452ee2272a
DjamelSlim <slimane.zeggari@gmail.com>
2018-07-29 16:01:39 +0200

    modif sur banche locale

--- Atelier/src/TestingUnit.java
+++ Atelier/src/TestingUnit.java
@@ -1,24 +1,19 @@
 import com.caci.Assurance;
 
 public class TestingUnit {
-    private String field;
-    private String commit1;
+  private String field;
+  private String commit1;
+  private String git;
+  private String autreChampgit;
 
-    private void method() {
-        System.out.print("autre modif");
-        System.out.print("modif dist");
-        Integer tata = 20;
-        System.out.print("modif autre");
-        System.out.print("en local");
+  private void method(){
+      System.out.print("autre modif");
+      System.out.print("modif dist");
+      Integer tata = 20;
+      System.out.print("modif autre");
+      System.out.print("en local");
 
-    }
-
-    public static void main(String[] args) {
-        Assurance assurance = new Assurance();
-        String s = assurance.MethodeRemout();
-        assurance.ajout("tata");
-
-    }
+  }
 
     public static void main(String[] args) {
         Assurance assurance = new Assurance();
@@ -29,9 +24,7 @@ public class TestingUnit {
     public String getField() {
         System.out.print("modif dans getField");
         System.out.print("autre modif");
-        System.out.print("autre modif");
-        System.out.print("modif sur aure_branche");
-
+      System.out.print("autre modif");
         return field;
     }
 
@@ -39,12 +32,12 @@ public class TestingUnit {
         this.field = field;
     }
 
-    private void toto() {
-        TestingUnit test = new TestingUnit();
+    private void toto(){
+      TestingUnit test = new TestingUnit();
 
 
-        System.out.print("tata");
-        System.out.println("remote");
+      System.out.print("tata");
+    System.out.println("remote");
 
-    }
+  }
 }
