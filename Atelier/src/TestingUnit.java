bfdf67c47c8145cc441c74a5d9d17253040ea5f1
DjamelSlim <slimane.zeggari@gmail.com>
2018-07-29 16:14:32 +0200

    ajout en local nouvelle methode

--- Atelier/src/TestingUnit.java
+++ Atelier/src/TestingUnit.java
@@ -1,22 +1,31 @@
 import com.caci.Assurance;
 
 public class TestingUnit {
-  private String field;
-  private String commit1;
+    private String field;
+    private String commit1;
 
-  private void method(){
-      System.out.print("autre modif");
-      System.out.print("modif dist");
-      Integer tata = 20;
-      System.out.print("modif autre");
-      System.out.print("en local");
+    private void method() {
+        System.out.print("autre modif");
+        System.out.print("modif dist");
+        Integer tata = 20;
+        System.out.print("modif autre");
+        System.out.print("en local");
 
-  }
+    }
+
+    public static void main(String[] args) {
+        Assurance assurance = new Assurance();
+        String s = assurance.MethodeRemout();
+        assurance.ajout("tata");
+
+    }
 
     public String getField() {
         System.out.print("modif dans getField");
         System.out.print("autre modif");
-      System.out.print("autre modif");
+        System.out.print("autre modif");
+        System.out.print("modif sur aure_branche");
+
         return field;
     }
 
@@ -24,12 +33,12 @@ public class TestingUnit {
         this.field = field;
     }
 
-    private void toto(){
-      TestingUnit test = new TestingUnit();
+    private void toto() {
+        TestingUnit test = new TestingUnit();
 
 
-      System.out.print("tata");
-    System.out.println("remote");
+        System.out.print("tata");
+        System.out.println("remote");
 
-  }
+    }
 }
