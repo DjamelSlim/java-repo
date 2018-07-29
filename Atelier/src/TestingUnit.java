73437449f8cfd3e42dcc8afa8b8ab28c1d3cbd19
DjamelSlim <slimane.zeggari@gmail.com>
2018-07-29 16:12:10 +0200

    suppression de public static void main

--- Atelier/src/TestingUnit.java
+++ Atelier/src/TestingUnit.java
@@ -15,12 +15,6 @@ public class TestingUnit {
 
   }
 
-    public static void main(String[] args) {
-        Assurance assurance = new Assurance();
-        String s = assurance.MethodeRemout();
-        assurance.ajout("tata");
-
-    }
     public String getField() {
         System.out.print("modif dans getField");
         System.out.print("autre modif");
